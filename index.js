baf190b5138b6709db0676c48a3b74cb81ec9d75
angleman <angleman@cormea.com>
2014-03-12 14:51:28 -0600

    readonly in stream

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 // in-stream
 fs    = require('fs')
 argv  = argv = require('minimist')(process.argv.slice(2))
+if (argv.f) {
+	console.log('instream:', argv.f)
+} else {
+	console.log('instream: stdin')
+}
 
-inStream = (argv.f) ? require('fs').createReadStream(argv.f) : process.stdin
+options = { 
+  flags:     'r',
+  encoding:  'utf8',
+  mode:      0444,
+  autoClose: true
+}
+
+inStream = (argv.f) ? fs.createReadStream(argv.f, options) : process.stdin
 
 module.exports = inStream
